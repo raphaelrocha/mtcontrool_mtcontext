ee719ef2f9129b54485f07436c8182e6bbff458b
Raphael Rocha <rli@manaus.certi.org.br>
2015-10-07 17:58:09 -0400

    case

--- protected/views/element/update_p2.php
+++ protected/views/element/update_p2.php
@@ -10,7 +10,7 @@
 
 
 
-<div class="infoblock shadow"><h1 style="color:#20B2AA; ">Update Element - Select Devices *<?php echo $model->DESCRIPTION; ?> </h1></div>
+<div class="infoblock shadow"><h1 style="color:#20B2AA; ">Update Element - Select Devices *<?php echo $model->description; ?> </h1></div>
 <HR WIDTH=1180 ALIGN=LEFT >
 
 
